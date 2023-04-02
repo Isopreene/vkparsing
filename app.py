01eefcf9e32be069322f5c18cb76c2f813dfdf02
Isopreene <andko2013@gmail.com>
2023-04-02 17:39:07 +0300

    1.1.1

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import main
+application = Flask(__name__)
+application.config["JSON_SORT_KEYS"] = False
+
+
+@application.route('/', methods=['get'])
+def get_groups_by_get_requests():
+    """Выводит все посты из указанных групп через гет-запросы"""
+    groups = request.args.get('groups', None)
+    all_ = request.args.get('all', None)
+    date_start = request.args.get('date_start', None)
+    date_finish = request.args.get('date_finish', None)
+    if all_ or not groups:
+        data = main.Runners.start_all(date_start=date_start, date_finish=date_finish)
+        return data
+    elif groups:
+        groups = groups.split(',')
+        data = main.Runners.start_group(*groups, date_start=date_start, date_finish=date_finish)
+        return data
+    else:
+        return f'Привет! Это парсер для групп vk.com. Пошлите get-запрос вида domain.com/?params или domain.com/params. \n\nСписок доступных параметров: groups=(через запятую), all=(True/False), date_start=(YYYY-MM-DD), date_finish=(YYYY-MM-DD)'
+
+
+@application.route('/all')
+def get_all_groups():
+    """Выводит все посты из всех групп, записанных в базу данных"""
+    data = main.Runners.start_all()
+    return data
+
+
+if __name__ == 'app':
+    application.run()
