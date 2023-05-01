3c51f0ea0794e64b6f88648635eafe6e78ca9f2c
Isopreene <andko2013@gmail.com>
2023-05-01 15:27:30 +0300

    1.5

--- backend/app.py
+++ backend/app.py
@@ -66,16 +66,16 @@ def make_celery(app):
     return celery
 
 workdir = getcwd()
-# arguments_db = KeysFromFiles().get_db()
-# arguments_vk = KeysFromFiles().get_vk() #данные для вк
-# arguments_captcha = KeysFromFiles().get_captcha() #данные для решения капчи
-# arguments_cloud = KeysFromFiles().get_cloud
+arguments_db = KeysFromFiles().get_db()
+arguments_vk = KeysFromFiles().get_vk() #данные для вк
+arguments_captcha = KeysFromFiles().get_captcha() #данные для решения капчи
+arguments_cloud = KeysFromFiles().get_cloud
 
 # для локальной базы
-arguments_db = {'host': 'localhost', 'port': 3306, 'user': 'root', 'password': '12august', 'database': 'vk', 'client_flag': CLIENT.MULTI_STATEMENTS}
-arguments_vk = {"token": "fbd44e02fbd44e02fbd44e022ff8c62d19ffbd4fbd44e029807b26b8f927d40b91c66a9"}
-arguments_captcha = {"server": "rucaptcha.com", "apiKey": "9b74cd2841f2e078d5e8e21cff3df6d8", "defaultTimeout": 120, "recaptchaTimeout": 600, "pollingInterval": 10}
-aruments_cloud = 'y0_AgAAAAAICbaeAAnQoQAAAADhyumQnrDWPKq4RJaNvozS0MynI_nnHew'
+# arguments_db = {'host': 'localhost', 'port': 3306, 'user': 'root', 'password': '12august', 'database': 'vk', 'client_flag': CLIENT.MULTI_STATEMENTS}
+# arguments_vk = {"token": "fbd44e02fbd44e02fbd44e022ff8c62d19ffbd4fbd44e029807b26b8f927d40b91c66a9"}
+# arguments_captcha = {"server": "rucaptcha.com", "apiKey": "9b74cd2841f2e078d5e8e21cff3df6d8", "defaultTimeout": 120, "recaptchaTimeout": 600, "pollingInterval": 10}
+# arguments_cloud = 'y0_AgAAAAAICbaeAAnQoQAAAADhyumQnrDWPKq4RJaNvozS0MynI_nnHew'
 
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
@@ -84,12 +84,9 @@ app.debug = True
 logger = get_task_logger(__name__)
 celery = make_celery(app)
 
-def link_to_cloud():
-    print("Task complete!")
-
 @celery.task(name = "periodic_task")
 def periodic_task():
-    print('Hi! from periodic_task')
+    main.Runners.upload_all(token=arguments_cloud, args_db=arguments_db)
     logger.info("Hello! from periodic task")
 
 
